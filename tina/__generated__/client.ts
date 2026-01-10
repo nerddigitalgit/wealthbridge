e4f28c4b153785c59bb4d2e4379532d1204584f2
marcio <marcio@nerddigital.com>
2026-01-09 22:55:10 -0500

    Fresh TinaCMS build with new client ID f7fcd1ba

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/mnt/c/Users/marci/Git/wealthbridge/tina/__generated__/.cache/1768015858443', url: 'https://content.tinajs.io/1.6/content/f7fcd1ba-07f9-4d24-afb1-0b26766873e5/github/main', token: 'e2d4ea160ea78f47cfd51295cfbf9ea6d2addb2e', queries,  });
+export const client = createClient({ cacheDir: '/mnt/c/Users/marci/Git/wealthbridge/tina/__generated__/.cache/1768017093452', url: 'https://content.tinajs.io/1.6/content/f7fcd1ba-07f9-4d24-afb1-0b26766873e5/github/main', token: 'placeholder', queries,  });
 export default client;
   
